3f785ca74260ba7086e535d02d6005eb4f6528ae
SuezDevelopment <olayode.gideon@calebuniversity.edu.ng>
2024-07-11 04:16:12 +0100

    fix(color-theme)

--- src/components/root.tsx
+++ src/components/root.tsx
@@ -49,7 +49,7 @@ const RootLayout: NextPage<PropsWithChildren<SeoMetaData>> = memo(({
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
-        <meta name="theme-color" content="#56ab2f" />
+        <meta name="theme-color" content="#14411a" />
       </Head>
       <main lang="en">
         {children}
