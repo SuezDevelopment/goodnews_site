8ef4a2e2d87146a9a26562a474d23a98c565a6a7
SuezDevelopment <olayode.gideon@calebuniversity.edu.ng>
2024-07-14 01:22:56 +0100

    added fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,17 @@
+import { fontSans } from "@/lib/fonts";
+import { cn } from "@/lib/utils";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" suppressHydrationWarning>
       <Head />
-      <body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
         <Main />
         <NextScript />
       </body>
