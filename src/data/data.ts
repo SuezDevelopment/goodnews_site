f01e0d05764582cf6f1b9a5b057ea3fe3bb59615
SuezDevelopment <olayode.gideon@calebuniversity.edu.ng>
2024-07-14 01:06:06 +0100

    better seo

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,4 +1,4 @@
 export const seoMetaData: SeoMetaData = {
     title: 'GoodNews - Cooperative Thrift & Credit Society',
-    description: 'We empower members with affordable financial services, savings plans, loans & financial education to build a secure future.',
+    description: 'We empower members with affordable financial services, savings plans, loans & financial education to build a secure future. Join us to build a secure and prosperous future through community-driven cooperative banking.',
 }
